d0f5b5bc8d81d2664bfdb712e48c63ecb7b9affc
ChrisLam790 <christopherlam790@gmail.com>
2025-12-17 16:44:51 -0500

    simply class name combination

---
+++ src/lib/utils.js
@@ -0,0 +1,10 @@
+/*
+Simplify class name combinations
+*/
+
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs) => {
+  return twMerge(clsx(inputs));
+};
