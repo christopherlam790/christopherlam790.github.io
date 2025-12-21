72f82d1c05c1e27fc6b32d0ba63b55a9e887ac66
ChrisLam790 <christopherlam790@gmail.com>
2025-12-20 19:04:40 -0500

    boilerplate projects section; to add actual projects

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,100 @@
+import { ArrowRight, Github } from "lucide-react";
+
+const projects = [
+  {
+    id: 1,
+    title: "Proj 1 Title",
+    description: "Proj 1 desc",
+    image: "/projects/",
+    tags: ["Skill", "Skill"],
+    githubUrl: "https://www.google.com/?client=safari&channel=mac_bm",
+  },
+  {
+    id: 2,
+    title: "Proj 2 Title",
+    description: "Proj 2 desc",
+    image: "/projects/",
+    tags: ["Skill", "Skill"],
+    githubUrl: "#",
+  },
+  {
+    id: 3,
+    title: "Proj 3 Title",
+    description: "Proj 3 desc",
+    image: "/projects/",
+    tags: ["Skill", "Skill"],
+    githubUrl: "#",
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Featured
+          <span className="text-primary"> Projects</span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur ex
+          enim doloremque eveniet beatae modi.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+              <div className="h-48 overflow-hidden ">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+
+                <h3 className="text-xl font-semibold mb-2">
+                  {" "}
+                  {project.title}{" "}
+                </h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}{" "}
+                </p>
+
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.githubUrl}
+                      className="text-foreground/80 hover:text-primary tansition-colors duration-300"
+                      target="_blank">
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            className="custom-button w-fit flex items-center mx-auto g-2"
+            target="_blank"
+            href="https://github.com/christopherlam790">
+            Check My Github <ArrowRight size={16}></ArrowRight>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
